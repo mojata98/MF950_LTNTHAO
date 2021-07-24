c20ad512f0895cfb87db593a525fd1bbc86554d4
mojata98 <ngocthao991203@gmail.com>
2021-07-24 07:58:52 +0700

    Update bài tập JS ngày 24/07/2021

---
+++ JS/js/page/demojs.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    alert('hello world!');    
+})
+
+function loadData(){
+    // $.ajax(
+    //     {
+    //         url: "http://cukcuk.manhnv.net/v1/Employees",
+    //         method: "GET",
+    //     }
+    // ).done(function(res) {
+    //     var data = res; 
+    //     debugger;
+    //     // data.forEach(emplooyee => {
+    //     //     var tr = $(`<tr>
+    //     //                     <td class="text-align-left">NV-532408</td>
+    //     //                     <td class="text-align-left">Hồ Văn Linh</td>
+    //     //                     <td class="text-align-left">Nu</td>
+    //     //                     <td class="text-align-center">11/08/1978</td>
+    //     //                     <td class="text-align-left">0940458449</td>
+    //     //                     <td class="text-align-left" style="max-width: 100px">cuon@example.comjshbfajvdvnjsba vbirnisvnwiaghiv</td>
+    //     //                     <td class="text-align-left">Giám đốc</td>
+    //     //                     <td class="text-align-left">Phòng Ke Toan</td>
+    //     //                     <td class="text-align-right">17.739.391</td>
+    //     //                     <td class="text-align-left">Đang lam việc</td>
+    //     //                 </tr>`);
+    //     //     $('tbody').append(tr);
+    //     // });
+    // }).fail(function(res) {
+
+    // })
+    alert('loadData');
+}
